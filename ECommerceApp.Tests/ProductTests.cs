58cf1f5478ce48ae54456f0c87e63ccccc0483b6
asingh8945 <arshjotsingh8945@conestogac.on.ca>
2025-02-06 15:50:53 -0500

    Added ProductTests class

--- ECommerceApp.Tests/ProductTests.cs
+++ ECommerceApp.Tests/ProductTests.cs
@@ -4,7 +4,7 @@ using System;
 namespace ECommerceApp.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ProductTests
     {
         [TestMethod]
         public void TestMethod1()
